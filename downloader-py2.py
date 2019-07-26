d3a260b33732e6790ac13ed16c3321ebe67ef324
A_Asaker <39923238+a-asaker@users.noreply.github.com>
2019-07-27 00:03:13 +0300

    Update downloader-py2.py

--- downloader-py2.py
+++ downloader-py2.py
@@ -5,9 +5,10 @@ import urllib
 from selenium import webdriver
 from xvfbwrapper import Xvfb
 from time import time
+import os,sys
 
 def process(n_blocks,block_size,file_size):
-	print "\r\t -> [ %f %% ]" %(n_blocks*block_size/float(file_size)*100), 
+	print "\r\t -> [ %.2f %% ]" %(n_blocks*block_size/float(file_size)*100), 
 
 def get_url(yt_url):
 	savefromnet="https://en.savefrom.net/#url=%s&utm_source=youtube.com&utm_medium=short_domains&utm_campaign=ssyoutube.com"%yt_url
@@ -26,46 +27,85 @@ def get_vid_lst(save_url):
 		page_html=page_html.replace("&amp;","&")
 	return page_html
 
-yt_url=raw_input("Enter Youtube URL : ")
-save_url=get_url(yt_url)
-page_html=get_vid_lst(save_url)
+def main():
+	man=False
+	if len(sys.argv)<3:
+		man=True
+		print ' [**] Usage :  Downloader-2 [Quality] [Video URL]'
+		x=raw_input(' [**] Continue Maniually ?(y/n) : ')
+		if x.lower()=='y':
+			pass
+		elif x.lower()=='n':
+			os._exit(0)
+		else:
+			print ' [xx] Unknown Answer !'
+			os._exit(0)
 
-quality = []
-strt=0
-while 1:
-	q_start = page_html.find('data-quality="',strt)
-	if q_start != -1:
-		q_end=page_html.find('"',q_start+14)
-		new_quality=str(page_html[q_start+14:q_end])
-		strt=q_end
-		if new_quality not in quality:
-			quality.append(new_quality)
-		else: pass
-	else: break
 
-print " [*] Avilable Qualities : " 
-for i in quality:
-	print "\t -%s" %i
+	if man:
+		yt_url=raw_input("Enter Youtube URL : ")
+	else :
+		yt_url=sys.argv[2]
 
-chsn_qlty=raw_input("Choose Quality : ")
+	save_url=get_url(yt_url)
+	page_html=get_vid_lst(save_url)
 
-start_srch='data-quality="%s" data-type="'%chsn_qlty
-start=page_html.find(start_srch)+len(start_srch)+11
-end=page_html.find('"',start)+1
-url=page_html[start:end]
+	quality = []
+	strt=0
+	while 1:
+		q_start = page_html.find('data-quality="',strt)
+		if q_start != -1:
+			q_end=page_html.find('"',q_start+14)
+			new_quality=str(page_html[q_start+14:q_end])
+			strt=q_end
+			if new_quality not in quality:
+				quality.append(new_quality)
+			else: pass
+		else: break
+	if not quality:
+		return "no"
 
-ext=str(page_html[start-11:start-8])
+	if man:
+		print " [*] Avilable Qualities : " 
+		for i in quality:
+			print "\t -%s" %i
+		chsn_qlty=raw_input("Choose Quality : ")
+	else:
+		if sys.argv[1] in quality:
+			chsn_qlty=sys.argv[1]
+		else :
+			print " [*] Video Is Not Available In The Quality You've Choosen , Please Choose Another Quality ..."
+			print " [*] Avilable Qualities : " 
+			for i in quality:
+				print "\t -%s" %i
+			chsn_qlty=raw_input("Choose Quality : ")
 
-name_start=page_html.rfind('download=',start)+10
-name_end=page_html.find('.',name_start)
-file_name=page_html[name_start:name_end]+"("+chsn_qlty+"p)."+ext
-print " [*] Video To Download : %s "%file_name
+	start_srch='data-quality="%s" data-type="'%chsn_qlty
+	start=page_html.find(start_srch)+len(start_srch)+11
+	end=page_html.find('"',start)+1
+	url=page_html[start:end]
 
-# file_name=raw_input("Save With Name : ")
-start_time=time()
-print " [*] Downloading Progress :  "
-urllib.urlretrieve(url,file_name,process)
-end_time=time()
-downloading_time=round((end_time-start_time)/60,2)
-print "\n [*] Download Completed !"
-print " [*] Time Taken For Downloading <| %f Minutes |>"%downloading_time
+	ext=str(page_html[start-11:start-8])
+
+	name_start=page_html.rfind('download=',start)+10
+	name_end=page_html.find('.',name_start)
+	file_name=page_html[name_start:name_end]+"("+chsn_qlty+"p)."+ext
+	try:
+		if quality:
+			print " [*] Video To Download : %s "%file_name
+			start_time=time()
+			print " [*] Downloading Progress :  "
+			os.chdir("/home/user/Desktop")
+			urllib.urlretrieve(url,file_name,process)
+			end_time=time()
+			downloading_time=round((end_time-start_time)/60,2)
+			print "\n [*] Download Completed !"
+			print " [*] Time Taken For Downloading <| %f Minutes |>"%downloading_time
+			return
+	except:
+		main()
+
+if __name__ == '__main__':
+	q=main()
+	if q=="no":
+		main()
